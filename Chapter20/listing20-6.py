454e72df2699dadf9e797ac48701b8e03338c776
wuzhi <wuzhi2010@gmail.com>
2017-08-08 10:14:51 +0800

    PEP8

--- Chapter20/listing20-6.py
+++ Chapter20/listing20-6.py
@@ -1,23 +1,29 @@
-import sys, re
+import sys
+import re
 from handlers import *
 from util import *
 from rules import *
+
 
 class Parser:
     """
     A Parser reads a text file, applying rules and controlling a
     handler.
     """
+
     def __init__(self, handler):
         self.handler = handler
         self.rules = []
         self.filters = []
-    def addRule(self, rule):
+
+    def add_rule(self, rule):
         self.rules.append(rule)
-    def addFilter(self, pattern, name):
+
+    def add_filter(self, pattern, name):
         def filter(block, handler):
             return re.sub(pattern, handler.sub(name), block)
         self.filters.append(filter)
+
     def parse(self, file):
         self.handler.start('document')
         for block in blocks(file):
@@ -26,25 +32,29 @@ class Parser:
             for rule in self.rules:
                 if rule.condition(block):
                     last = rule.action(block, self.handler)
-                    if last: break
+                    if last:
+                        break
         self.handler.end('document')
+
 
 class BasicTextParser(Parser):
     """
     A specific Parser that adds rules and filters in its
     constructor.
     """
+
     def __init__(self, handler):
         Parser.__init__(self, handler)
-        self.addRule(ListRule())
-        self.addRule(ListItemRule())
-        self.addRule(TitleRule())
-        self.addRule(HeadingRule())
-        self.addRule(ParagraphRule())
+        self.add_rule(ListRule())
+        self.add_rule(ListItemRule())
+        self.add_rule(TitleRule())
+        self.add_rule(HeadingRule())
+        self.add_rule(ParagraphRule())
 
-        self.addFilter(r'\*(.+?)\*', 'emphasis')
-        self.addFilter(r'(http://[\.a-zA-Z/]+)', 'url')
-        self.addFilter(r'([\.a-zA-Z]+@[\.a-zA-Z]+[a-zA-Z]+)', 'mail')
+        self.add_filter(r'\*(.+?)\*', 'emphasis')
+        self.add_filter(r'(http://[\.a-zA-Z/]+)', 'url')
+        self.add_filter(r'([\.a-zA-Z]+@[\.a-zA-Z]+[a-zA-Z]+)', 'mail')
+
 
 handler = HTMLRenderer()
 parser = BasicTextParser(handler)
--- Chapter20/rules.py
+++ Chapter20/rules.py
@@ -4,11 +4,13 @@ class Rule:
     Base class for all rules.
     超类，所有规则类均有action函数，根据类型调用handler函数
     """
+
     def action(self, block, handler):
         handler.start(self.type)
         handler.feed(block)
         handler.end(self.type)
         return True
+
 
 class HeadingRule(Rule):
     """
@@ -17,8 +19,10 @@ class HeadingRule(Rule):
     heading 是单行 最大70字符 非: 结尾
     """
     type = 'heading'
+
     def condition(self, block):
-        return not '\n' in block and len(block) <= 70 and not block[-1] == ':'
+        return '\n' not in block and len(block) <= 70 and not block[-1] == ':'
+
 
 class TitleRule(HeadingRule):
     """
@@ -30,9 +34,11 @@ class TitleRule(HeadingRule):
     first = True
 
     def condition(self, block):
-        if not self.first: return False
+        if not self.first:
+            return False
         self.first = False
         return HeadingRule.condition(self, block)
+
 
 class ListItemRule(Rule):
     """
@@ -41,13 +47,16 @@ class ListItemRule(Rule):
     列表项： 以 - 为起点，格式化后，起点 - 去掉
     """
     type = 'listitem'
+
     def condition(self, block):
         return block[0] == '-'
+
     def action(self, block, handler):
         handler.start(self.type)
         handler.feed(block[1:].strip())
         handler.end(self.type)
         return True
+
 
 class ListRule(ListItemRule):
     """
@@ -58,8 +67,10 @@ class ListRule(ListItemRule):
     """
     type = 'list'
     inside = False
+
     def condition(self, block):
         return True
+
     def action(self, block, handler):
         if not self.inside and ListItemRule.condition(self, block):
             handler.start(self.type)
@@ -69,6 +80,7 @@ class ListRule(ListItemRule):
             self.inside = False
         return False
 
+
 class ParagraphRule(Rule):
     """
     A paragraph is simply a block that isn't covered by any of the
@@ -76,5 +88,6 @@ class ParagraphRule(Rule):
     段落是其他规则没有覆盖后的块
     """
     type = 'paragraph'
+
     def condition(self, block):
         return True
